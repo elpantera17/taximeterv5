54efa3a541b638cf61685890157355c2d245f92f
elpantera17 <p.joselopez17@gmail.com>
2025-07-16 12:34:45 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/project/',
+  base: '/taximeter/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
