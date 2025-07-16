091a81c6e019d1dd0e587deb41c54f119572f099
elpantera17 <p.joselopez17@gmail.com>
2025-07-16 11:08:45 -0400

    Add files via upload

---
+++ public/api/auth/login.php
@@ -0,0 +1,17 @@
+<?php
+require_once '../config.php';
+require_once '../controllers/UserController.php';
+
+header('Content-Type: application/json');
+$input = json_decode(file_get_contents('php://input'), true);
+
+if (!isset($input['email']) || !isset($input['password'])) {
+    echo json_encode(['success' => false, 'message' => 'Faltan credenciales']);
+    exit;
+}
+
+$userController = new UserController($db_config);
+$response = $userController->login($input['email'], $input['password']);
+
+echo json_encode($response);
+?>
